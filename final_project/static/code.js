3608545a29078761c954886aa762bd296c37ee5c
Nishijama <nathanplociennik@gmail.com>
2021-07-06 23:21:31 +0000

    Updated visuals and worked on adding JS to chart/table view and pages/requests view, dynamically assigned month

--- final_project/static/code.js
+++ final_project/static/code.js
@@ -1,20 +1,89 @@
+
+
+//default setup
+var type = 'requests';
+document.getElementById("reqButton").style.backgroundColor = "#00A9F4";
+
 var state = 'chart';
 document.getElementById("table/chart").innerHTML = 'Table view';
 
-//change text in navbar when state is changed
-//if we're in the chart state:
-function stateFunction () {
-    if (state == 'chart') {
+document.getElementById("prcImage").src="static/requestChart.png";
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var d = new Date();
+var n = d.getMonth();
+document.getElementById("date").innerHTML = months[n];
+
+//---------------
+//requests/pages settup
+
+document.getElementById('reqButton').addEventListener('click', function(){
+    document.getElementById("reqButton").style.backgroundColor = "#00A9F4";
+    document.getElementById("pagesButton").style.backgroundColor = "#051C2C";
+
+    if (state == 'chart'){
+        document.getElementById("prcImage").src="static/requestChart.png";
+    }
+    else if (state == 'table') {
+        document.getElementById("prcImage").src="static/requestTable.png";
+    }
+
+    type = 'requests';
+})
+
+document.getElementById('pagesButton').addEventListener('click', function() {
+    document.getElementById("pagesButton").style.backgroundColor = "#00A9F4";
+    document.getElementById("reqButton").style.backgroundColor = "#051C2C";
+
+    if (state == 'chart'){
+        document.getElementById("prcImage").src="static/pagesChart.png";
+    }
+    else if (state == 'table') {
+        document.getElementById("prcImage").src="static/pagesTable.png";
+    }
+
+    type = 'pages'
+})
+
+//-------------
+//table vs chart settup
+
+document.getElementById('table/chart').addEventListener('click', function() {
+
+    if (state == 'chart'){
+
         document.getElementById("table/chart").innerHTML = 'Chart view';
-        document.getElementById("prcImage").src="static/PRC-chart.jpg";
+
+        if (type == 'requests'){
+            document.getElementById("prcImage").src="static/requestTable.png";
+        }
+        else if (type == 'pages') {
+            document.getElementById("prcImage").src="static/pagesTable.png";
+        }
         state = 'table';
-    return;
     }
-    //if we're in the table state
-    else {
+
+    else if (state == 'table') {
+
         document.getElementById("table/chart").innerHTML = 'Table view';
-        document.getElementById("prcImage").src="static/toDoCat.jpg";
-    state = 'chart';
-    return;
+
+        if (type == 'requests'){
+            document.getElementById("prcImage").src="static/requestChart.png";
+        }
+        else if (type == 'pages') {
+            document.getElementById("prcImage").src="static/pagesChart.png";
+        }
+        state = 'chart';
     }
-}
+})
+
+//----------
+//Pop-up setup
+
+document.getElementById('editButton').addEventListener('click', function(){
+  document.querySelector('.modalBg').style.display = 'flex';
+})
+
+document.querySelector('.close').addEventListener('click', function() {
+  document.querySelector('.modalBg').style.display = 'none';
+})
