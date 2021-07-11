5286dc47f005ba53544d87574c5e467e6a60b72d
Nishijama <nathanplociennik@gmail.com>
2021-07-12 00:27:18 +0200

    created a hash function and worked on registration part

--- final_project/application.py
+++ final_project/application.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from cs50 import SQL
+from hash import make_pw_hash, check_pw_hash
+
 
 app = Flask(__name__)
 db = SQL("sqlite:///database.db")
@@ -15,12 +17,28 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        # check if user exists, check if pw is correct, redirect to the main page
+        return 0
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register new user"""
-    return render_template("register.html")
+    """Register new user""" 
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        name = request.form.get("name")
+        password = request.form.get("password")
+
+        #here we will hash the password
+        password_hash = make_pw_hash(password)
+        #
+        #
+
+        db.execute("INSERT INTO users (name, password_hash) VALUES(:name, :password_hash)", name = name, password_hash = password_hash)
 
 @app.route("/users", methods=["GET", "POST"])
 def users():
---
+++ final_project/hash.py
@@ -0,0 +1,10 @@
+import hashlib
+
+def make_pw_hash(password):
+	return hashlib.sha256(str.encode(password)).hexdigerst()
+
+def check_pw_hash(password, hash):
+	if make_pw_hash(password) == hash:
+		return True
+	
+	return False
