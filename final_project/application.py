35763ad009628e6611161a09b413fbdd5a999730
Nishijama <nathanplociennik@gmail.com>
2021-07-11 21:45:13 +0000

    Created registering page, css file for login and register page; created a page showing all registered users for debugging, started working on SQL database

--- final_project/application.py
+++ final_project/application.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from cs50 import SQL
 
 app = Flask(__name__)
+db = SQL("sqlite:///database.db")
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -9,3 +12,18 @@ def index():
     if request.method == "GET":
         return render_template("index.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register new user"""
+    return render_template("register.html")
+
+@app.route("/users", methods=["GET", "POST"])
+def users():
+    """Display registered users for debugging"""
+    rows = db.execute("SELECT * FROM users")
+    return render_template("users.html", rows=rows)
--- final_project/static/code.js
+++ final_project/static/code.js
@@ -15,6 +15,26 @@ var n = d.getMonth();
 document.getElementById("date").innerHTML = months[n];
 
 //---------------
+//date settup
+
+document.getElementById('prevMonth').addEventListener('click', function(){
+    n -= 1
+    if (n < 0) {
+        n = 11;
+    }
+
+    document.getElementById("date").innerHTML = months[n]
+})
+
+document.getElementById('nextMonth').addEventListener('click', function(){
+    n += 1
+    if (n > 11) {
+        n = 0;
+    }
+    document.getElementById("date").innerHTML =  months[n];
+})
+
+//---------------
 //requests/pages settup
 
 document.getElementById('reqButton').addEventListener('click', function(){
